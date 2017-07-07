8357be0e078939e12721241bb0270991eeff23e1
nickcharron <ncharron@clearpathrobotics.com>
2017-07-07 17:09:05 -0400

    configuring imu

--- outdoor_waypoint_nav/run.sh
+++ outdoor_waypoint_nav/run.sh
@@ -3,5 +3,7 @@
 set -e #exit on first error
 
 export HUSKY_TOP_PLATE_ENABLED="false"
+export HUSKY_IMU_XYZ="0 0.17 0.07"
+export HUSKY_IMU_RPY="3.14159 0 0"
 
 roslaunch outdoor_waypoint_nav outdoor_waypoint_nav.launch
