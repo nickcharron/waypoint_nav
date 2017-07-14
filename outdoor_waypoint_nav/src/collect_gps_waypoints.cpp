0f0858024e0b68298cb9f56fbfa91564b8ed4eba
Nicholas Charron <ncharron@clearpathrobotics.com>
2017-07-14 11:24:30 -0400

    fixed timing issues with waypoint collection

--- outdoor_waypoint_nav/src/collect_gps_waypoints.cpp
+++ outdoor_waypoint_nav/src/collect_gps_waypoints.cpp
@@ -4,54 +4,55 @@
 #include <iostream>
 #include <sensor_msgs/NavSatFix.h>
 #include <sensor_msgs/Joy.h>
-#include <math.h>
 #include <ros/duration.h>
 #include <ros/time.h>
 
 bool collect_request = false;
-bool end_collection = false;
+bool continue_collection = true;
 double lati_point=0, longi_point=0;
 std::string end_button_sym, collect_button_sym;
 int end_button_num = 0, collect_button_num = 0;
 
 void joy_CB(const sensor_msgs::Joy joy_msg)
 {
-	
 	if(joy_msg.buttons[collect_button_num]==1)
+	//if(joy_msg.buttons[collect_button_num]==1)
 	{
 		collect_request = true;
+
 	}	
 	if(joy_msg.buttons[end_button_num]==1)
+	//if(joy_msg.buttons[end_button_num]==1)
 	{
-		end_collection = true;
+		ROS_INFO("end request registered");
+		continue_collection = false;
 	}
 }
+
 void filtered_gps_CB(const sensor_msgs::NavSatFix gps_msg)
 {
-	if(collect_request == true)
-	{
 		lati_point = gps_msg.latitude;
 		longi_point = gps_msg.longitude;
-	}
 }
 
 int main(int argc, char** argv)
 {
-
 	//Initialize variables
 		int numWaypoints = 0;
 		std::string path_local;
 
-    // Initialize node
+    // Initialize node and time
 		ros::init(argc, argv, "collect_gps_waypoints"); //initiate node called collect_gps_waypoints
 		ros::NodeHandle n;
+		ros::Time::init();
+		ros::Rate rate(1);
 
 	// Get button numbers to collect waypoints and end collection
 		ros::param::get("collect_button_num", collect_button_num);
 		ros::param::get("end_button_num", end_button_num);
 
     //Initiate subscribers
-		ros::Subscriber sub_joy = n.subscribe("/joy", 100, joy_CB);
+		ros::Subscriber sub_joy = n.subscribe("/joy_teleop/joy", 100, joy_CB);
 		ros::Subscriber sub_gps = n.subscribe("/gps/filtered", 100, filtered_gps_CB);
 		ROS_INFO("Initiated collect_gps_waypoints node");
 
@@ -66,30 +67,34 @@ int main(int argc, char** argv)
 		ros::param::get("end_button_sym", end_button_sym);
 		ROS_INFO("Press %s button to collect and store waypoint.", collect_button_sym.c_str());
 		ROS_INFO("Press %s button to end waypoint collection.", end_button_sym.c_str());
+		std::cout << std::endl;
 
-		if(coordFile.is_open())
+	if(coordFile.is_open())
+	{
+		while(continue_collection)
+		{
+			ros::spinOnce();
+			if(collect_request == true)
 			{
-				while(ros::ok() && !end_collection)
-				{
-					if(collect_request == true)
-					{
-						coordFile << lati_point << " " << longi_point << std::endl;
-						ROS_INFO("You have collected another waypoint!");
-						ROS_INFO("Press %s button to collect and store another waypoint.", collect_button_sym.c_str());
-						ROS_INFO("Press %s button to end waypoint collection.", end_button_sym.c_str());
-						numWaypoints++;
-						collect_request = false; //reset
-					}
-				}
-				coordFile.close();
+				coordFile << lati_point << " " << longi_point << std::endl;
+				ROS_INFO("You have collected another waypoint!");
+				ROS_INFO("Press %s button to collect and store another waypoint.", collect_button_sym.c_str());
+				ROS_INFO("Press %s button to end waypoint collection.", end_button_sym.c_str());
+				std::cout << std::endl;
+				numWaypoints++;
+				collect_request = false; //reset
 			}
-		else
-			{
-				ROS_ERROR("Unable to open file.");
-				ROS_INFO("Exiting..");
-			}
+			rate.sleep();
+		}
+		coordFile.close();
+	}
+	else
+	{
+		ROS_ERROR("Unable to open file.");
+		ROS_INFO("Exiting..");
+	}
 
-	ROS_INFO("Closed waypoint file, you have collected %d points", numWaypoints);
+	ROS_INFO("Closed waypoint file, you have collected %d waypoints", numWaypoints);
 	ROS_INFO("Ending node...");
 	ros::shutdown();
 	return 0;
